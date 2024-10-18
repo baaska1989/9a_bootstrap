b81178a6977da561f241fc3aef8eb16a22cb094d
Baaska Lhagbasuren <baaska1989@gmail.com>
2024-10-18 09:59:21 +0800

    add sql

---
+++ bgd.sql
@@ -0,0 +1,16 @@
+
+--Үйл ажиллагааны чиглэлээр ангилж байгаа SQL
+SELECT service.name, count(customers.service_id) as total
+FROM customers 
+INNER JOIN service 
+ON customers.service_id = service.id 
+GROUP BY customers.service_id 
+ORDER BY total DESC;
+
+
+-- Хороогоор ангилж байгаа SQL
+SELECT customer_horoo.name as horoo, count(*) as total
+FROM customers 
+INNER JOIN customer_horoo 
+ON customers.address = customer_horoo.id 
+GROUP BY customers.address;
